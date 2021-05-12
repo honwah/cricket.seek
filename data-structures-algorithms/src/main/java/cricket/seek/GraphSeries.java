2530ca07b4ddbb222115b8d2dc61faf5d573ccde
ethan wang <tdwwwhh@163.com>
2021-05-12 21:23:32 +0800

    '图'

--- data-structures-algorithms/src/main/java/cricket/seek/GraphSeries.java
+++ data-structures-algorithms/src/main/java/cricket/seek/GraphSeries.java
@@ -1,7 +1,7 @@
 package cricket.seek;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
+import java.util.*;
 
 /**
  * 关于图的求解
@@ -17,6 +17,9 @@ public class GraphSeries {
     public enum GraphType {
         DIRECTED, UNDIRECTED
     }
+
+    //是否已访问顶点
+    private boolean[] isVisited;
 
     //图的邻接矩阵表示
     class AdjacencyMatrixGraph<T extends Comparable<T>> {
@@ -46,6 +49,60 @@ public class GraphSeries {
             }
             for (int i = 0; i < edges.length; i++) {
                 insertEdge(edges[i]);
+            }
+        }
+
+        //获取图中的边
+        private Edge[] getEdges() {
+            int index = 0;
+            Edge[] edges = new Edge[numOfEdges];
+            for (int i = 0; i < vertices.size(); i++) {
+                for (int j = 0; j < vertices.size(); j++) {
+                    if (i != j && matrix[i][j] != MAX_WEIGHT) {
+                        edges[index++] = new Edge(i, j, matrix[i][j]);
+                    }
+                }
+            }
+            return edges;
+        }
+
+        //对边按照权值大小进行排序(由小到大)
+        private void sortEdges(Edge[] edges, int elen) {
+            for (int i = 0; i < elen; i++) {
+                for (int j = i + 1; j < elen; j++) {
+                    if (edges[i].weight > edges[j].weight) {
+                        // 交换"边i"和"边j"
+                        Edge tmp = edges[i];
+                        edges[i] = edges[j];
+                        edges[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        //获取节点a属于哪个集合，这里获取a根节点编号，根节点编号代表所属集合
+        public int find(int[] id, int element) {
+            while (element != id[element]) {
+                element = id[element];
+            }
+            return element;
+        }
+
+        //合并a顶点和b顶点所在集合，判断顶点a和顶点b的根节点大小所在集合大小，将节点少的集合的节点添加到节点多的集合上
+        public void union(int[] id, int[] weight, int firstElement, int secondElement) {
+            int firstRoot = find(id, firstElement);
+            int secondRoot = find(id, secondElement);
+            //如果已经属于同一个集合了，就不用再合并了。
+            if (firstRoot == secondRoot) {
+                return;
+            }
+
+            if (weight[firstRoot] > weight[secondRoot]) {
+                id[secondRoot] = firstRoot;
+                weight[firstRoot] += weight[secondRoot];
+            } else {
+                id[firstRoot] = secondRoot;
+                weight[secondRoot] += weight[firstRoot];
             }
         }
 
@@ -156,6 +213,154 @@ public class GraphSeries {
             }
         }
 
+        //深度优先遍历
+        public void dfsTraverse(T vertex)//要遍历的初始顶点
+        {
+            isVisited = new boolean[vertices.size()];//初始化顶点是否遍历
+            if (vertices.size() > 0) {
+                Stack<T> stack = new Stack<T>();
+                stack.push(vertex);
+                while (!stack.empty()) {
+                    T cur = stack.peek();
+                    int idx = vertices.indexOf(cur);
+                    if (isVisited[idx] == false) {
+                        System.out.print(cur);
+                        isVisited[idx] = true;
+                    }
+                    int j = 0;
+                    for (; j < vertices.size(); ++j)
+                        if (idx != j && matrix[idx][j] != MAX_WEIGHT && isVisited[j] == false) {
+                            stack.push(vertices.get(j));
+                            break;
+                        }
+                    //没有未访问过的顶点时
+                    if (j == vertices.size())
+                        stack.pop();//回到上一个顶点，继续试探
+                }
+                System.out.print(System.getProperty("line.separator"));
+            }
+        }
+
+        //广度优先搜索
+        public void bfsTraverse(T vertex) { //要遍历的初始顶点
+            isVisited = new boolean[vertices.size()];//初始化顶点是否遍历
+            if (vertices.size() > 0) {
+                Queue<T> queue = new LinkedList<T>();
+                queue.offer(vertex);
+                while (!queue.isEmpty()) {
+                    T cur = queue.poll();
+                    int idx = vertices.indexOf(cur);
+                    if (isVisited[idx] == false) {
+                        System.out.print(cur);
+                        isVisited[idx] = true;
+                    }
+                    for (int j = 0; j < vertices.size(); ++j)
+                        if (idx != j && matrix[idx][j] != MAX_WEIGHT && isVisited[j] == false) {
+                            T v = vertices.get(j);
+                            if (!queue.contains(v)) {
+                                queue.offer(v);
+                            }
+                        }
+                }
+                System.out.print(System.getProperty("line.separator"));
+            }
+        }
+
+        // Dijkstra算法实现到某顶点的最短路径
+        public void dijkstra(T startpoint) {//起始顶点
+            int startindex = vertices.indexOf(startpoint);
+            ArrayList<ShortestPath> tArray = new ArrayList<ShortestPath>();//T集合中存找到最短路径的对象
+            ArrayList<ShortestPath> sArray = new ArrayList<ShortestPath>();//S集合中存未找到最短路径的对象
+            //初始化S列表
+            for (int i = 0; i < vertices.size(); i++) {
+                ShortestPath item = new ShortestPath();
+                item.end = vertices.get(i);
+                item.pathArray.add(vertices.get(startindex));
+                item.pathArray.add(vertices.get(i));
+                item.distance = matrix[startindex][i];
+                sArray.add(item);
+            }
+            while (!sArray.isEmpty()) {
+                //S集合中找距离起始顶点最近的顶点
+                int minIndex = 0;
+                int tempDistance = sArray.get(0).distance;
+                for (int i = 0; i < sArray.size(); i++) {
+                    if (tempDistance > sArray.get(i).distance) {
+                        minIndex = i;
+                        tempDistance = sArray.get(i).distance;
+                    }
+                }
+                ShortestPath goal = sArray.remove(minIndex);//找到最短路径
+                tArray.add(goal);
+                //考虑新找到的最短路径顶点是否会影响S集合其他顶点，通过“边”来松弛起始顶点到其余各个顶点的路程
+                int index1 = vertices.indexOf(goal.end);//已找到最短路径的顶点下标
+                for (int i = 0; i < sArray.size(); i++) {
+                    int index2 = vertices.indexOf(sArray.get(i).end);//未找到最短路径顶点下标
+                    if (matrix[index1][index2] == MAX_WEIGHT) {//两顶点没有路径
+                        continue;
+                    } else {
+                        int newdistance = goal.distance + matrix[index1][index2];
+                        //新找到最短路径的顶点影响了路径长度，导致顶点比原来路径值小
+                        if (newdistance < sArray.get(i).distance) {
+                            //更新路径长度
+                            sArray.get(i).distance = newdistance;
+                            //更新路径
+                            sArray.get(i).pathArray = new ArrayList<T>(goal.pathArray);
+                            sArray.get(i).pathArray.add(sArray.get(i).end);
+                        }
+                    }
+                }
+            }
+            //打印最短路径
+            for (int i = 0; i < tArray.size(); i++) {
+                if (tArray.get(i).distance == MAX_WEIGHT) {
+                    System.out.println(startpoint + "-->" + tArray.get(i).end + "的最短路径为: 无" + "   长度为: ∞");
+                } else {
+                    System.out.print(startpoint + "-->" + tArray.get(i).end + "的最短路径为:");
+                    for (int j = 0; j < tArray.get(i).pathArray.size(); j++) {
+                        System.out.print(tArray.get(i).pathArray.get(j) + "  ");
+                    }
+                    System.out.println("长度为:" + tArray.get(i).distance);
+                }
+            }
+        }
+
+        //克鲁斯卡尔（Kruskal)最小生成树
+        public void kruskal() {
+            ArrayList<Edge> rtlist = new ArrayList<Edge>();
+            int[] id = new int[vertices.size()];//表示并查集所有元素
+            int[] weight = new int[vertices.size()];//表示集合元素数量
+            for (int i = 0; i < vertices.size(); i++) {
+                id[i] = i;        //初始时数组内的值与数组的下角标一致。即每个数字都自成立一个集合
+                weight[i] = 1;    //初始集合大小1
+            }
+            //获取"图中所有的边"
+            Edge[] edges = getEdges();
+            // 将边按照"权"的大小进行排序(从小到大)
+            sortEdges(edges, numOfEdges);
+            int count = 0;
+            for (int i = 0; i < edges.length; i++) {
+                int p1 = edges[i].from;      // 获取第i条边的"起点"的下标
+                int p2 = edges[i].to;        // 获取第i条边的"终点"的下标
+                int ida = find(id, p1);
+                int idb = find(id, p2);
+                if (ida != idb) {
+                    rtlist.add(edges[i]);
+                    count++;
+                    union(id, weight, p1, p2);
+                }
+                if (count >= vertices.size() - 1)
+                    break;
+            }
+            // 统计并打印"kruskal最小生成树"的信息
+            int length = 0;
+            for (int i = 0; i < rtlist.size(); i++)
+                length += rtlist.get(i).weight;
+            System.out.printf("Kruskal=%d: ", length);
+            for (int i = 0; i < rtlist.size(); i++)
+                System.out.printf("(%s->%s:%s) ", vertices.get(rtlist.get(i).from), vertices.get(rtlist.get(i).to), rtlist.get(i).weight);
+            System.out.printf("\n");
+        }
 
         @Override
         public String toString() {//打印矩阵
@@ -165,6 +370,13 @@ public class GraphSeries {
                 sb.append(Arrays.toString(edge) + System.getProperty("line.separator"));
             }
             return sb.toString().replaceAll(String.valueOf(MAX_WEIGHT), "∞");
+        }
+
+        //最短路径
+        class ShortestPath {
+            T end;//终点
+            ArrayList<T> pathArray = new ArrayList<T>();//到终点的顶点集合
+            int distance;//路径长度
         }
     }
 
--- data-structures-algorithms/src/test/java/cricket/seek/GraphSeriesTest.java
+++ data-structures-algorithms/src/test/java/cricket/seek/GraphSeriesTest.java
@@ -1,6 +1,7 @@
 package cricket.seek;
 
 import cricket.seek.GraphSeries.AdjacencyMatrixGraph;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -18,6 +19,7 @@ public class GraphSeriesTest {
 
     public static class AdjacencyMatrixGraphTest {
 
+        @Ignore("已验证测试")
         @Test
         public void test() {
             String[] vertices = {"A", "B", "C", "D", "E"};
@@ -46,6 +48,75 @@ public class GraphSeriesTest {
             amg.delVertex("G");
             System.out.println("边个数："+amg.getNumOfEdges());
             System.out.println(amg);
+        }
+
+        @Ignore("已验证测试")
+        @Test
+        public void testDFS() {
+            String[] vertices = {"A", "B", "C", "D", "E","F","G","H"};
+            Edge[] edges = {new Edge(0, 1, 10), new Edge(0, 2, 7),
+                    new Edge(0, 5, 7), new Edge(2, 3, 6),
+                    new Edge(1, 5, 8), new Edge(2, 1, 3),
+                    new Edge(5, 7, 6), new Edge(7, 6, 8),
+                    new Edge(6, 1, 3), new Edge(6, 2, 9),
+                    new Edge(6, 4, 9)
+            };
+            AdjacencyMatrixGraph<String> amg = gs.new AdjacencyMatrixGraph<>(vertices, edges, GraphSeries.GraphType.DIRECTED);
+            System.out.println("顶点个数："+amg.getNumOfVertices());
+            System.out.println("边个数："+amg.getNumOfEdges());
+            System.out.println(amg);
+            amg.dfsTraverse("A");
+        }
+
+        @Ignore("已验证测试")
+        @Test
+        public void testBFS() {
+            String[] vertices = {"A", "B", "C", "D", "E","F","G","H"};
+            Edge[] edges = {new Edge(0, 1, 10), new Edge(0, 2, 7),
+                    new Edge(0, 5, 7), new Edge(2, 3, 6),
+                    new Edge(1, 5, 8), new Edge(2, 1, 3),
+                    new Edge(5, 7, 6), new Edge(7, 6, 8),
+                    new Edge(6, 1, 3), new Edge(6, 2, 9),
+                    new Edge(6, 4, 9)
+            };
+            AdjacencyMatrixGraph<String> amg = gs.new AdjacencyMatrixGraph<>(vertices, edges, GraphSeries.GraphType.DIRECTED);
+            System.out.println("顶点个数："+amg.getNumOfVertices());
+            System.out.println("边个数："+amg.getNumOfEdges());
+            System.out.println(amg);
+            amg.bfsTraverse("A");
+        }
+
+        @Ignore("已验证测试")
+        @Test
+        public void testdijkstra() {
+            String[] vertices = {"V1", "V2", "V3", "V4", "V5","V6"};
+            Edge[] edges = {new Edge(0, 2, 10), new Edge(0, 4, 30),
+                    new Edge(0, 5, 100), new Edge(1, 2, 5),
+                    new Edge(2, 3, 50), new Edge(3, 5, 10),
+                    new Edge(4, 3, 20), new Edge(4, 5, 60)
+            };
+            AdjacencyMatrixGraph<String> amg = gs.new AdjacencyMatrixGraph<>(vertices, edges, GraphSeries.GraphType.DIRECTED);
+            System.out.println("顶点个数："+amg.getNumOfVertices());
+            System.out.println("边个数："+amg.getNumOfEdges());
+            System.out.println(amg);
+            amg.dijkstra("V1");
+        }
+
+        @Test
+        public void testkruskal() {
+            String[] vertices = {"A", "B", "C", "D", "E","F","G"};
+            Edge[] edges = {new Edge(0, 1, 12), new Edge(0, 5, 16),
+                    new Edge(0, 6, 14), new Edge(1, 2, 10),
+                    new Edge(1, 5, 7), new Edge(3, 2, 3),
+                    new Edge(2, 4, 5), new Edge(4, 3, 4),
+                    new Edge(2, 5, 6),new Edge(5, 4, 2),
+                    new Edge(5, 6, 9),new Edge(4, 6, 8)
+            };
+            AdjacencyMatrixGraph<String> amg = gs.new AdjacencyMatrixGraph<>(vertices, edges, GraphSeries.GraphType.DIRECTED);
+            System.out.println("顶点个数："+amg.getNumOfVertices());
+            System.out.println("边个数："+amg.getNumOfEdges());
+            System.out.println(amg);
+            amg.kruskal();
         }
     }
 }
